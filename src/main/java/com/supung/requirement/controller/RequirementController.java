7cce96e46faf7e958d0dd26b302ea9de1ed4426e
supun_g <supun.g@synergenhealth.com>
2022-05-19 10:21:57 +0530

    gatewayserver

--- src/main/java/com/supung/requirement/controller/RequirementController.java
+++ src/main/java/com/supung/requirement/controller/RequirementController.java
@@ -4,11 +4,13 @@ import com.supung.requirement.model.dto.RequirementInputDTO;
 import com.supung.requirement.model.dto.RequirementOutputDTO;
 import com.supung.requirement.service.RequirementService;
 import com.supung.requirement.service.RequirementServiceMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/requirements")
 public class RequirementController {
@@ -34,7 +36,9 @@ public class RequirementController {
     }
 
     @GetMapping("getByOrphanageId/{orphanageId}")
-    public ResponseEntity<List<RequirementOutputDTO>> findAllByOrphanageId(@PathVariable("orphanageId") long orphanageId) {
+    public ResponseEntity<List<RequirementOutputDTO>> findAllByOrphanageId(@RequestHeader("correlation-id") String correlationId,
+                                                                           @PathVariable("orphanageId") long orphanageId) {
+        log.info("Finding requirements by orphanage id : {}, correlationId : {}", orphanageId, correlationId);
         return ResponseEntity.ok(requirementService.findAllByOrphanageId(orphanageId));
     }
 
